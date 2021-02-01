10ecf85719897c296d373f750a9557cbc430a9f7
coldfarmer <76575201+coldfarmer@users.noreply.github.com>
2021-02-02 00:07:17 +0800

    11
    11

--- js/slide.js
+++ js/slide.js
@@ -16,18 +16,24 @@ $(document).ready(function(){
     $('.Adv_bar').eq(0).css('background-color','rgba(11, 171, 177, 0.815)');
     $('.Adv_bar').eq(0).click(function () {
         $('.slide_1').css('margin-left', '0%' );
+        $('.slide_2').css('margin-left', '0%' );
+        $('.slide_3').css('margin-left', '0%' );
         $('.Adv_bar').eq(0).css('background-color','rgba(11, 171, 177, 0.815)');
         $('.Adv_bar').eq(1).css('background-color','#6e6d6d');
         $('.Adv_bar').eq(2).css('background-color','#6e6d6d');
       });
      $('.Adv_bar').eq(1).click(function () {
-        $('.slide_1').css('margin-left', '-66.5%' );
+        $('.slide_1').css('margin-left', '-100%' );
+        $('.slide_2').css('margin-left', '-100%' );
+        $('.slide_3').css('margin-left', '-100%' );
         $('.Adv_bar').eq(1).css('background-color','rgba(11, 171, 177, 0.815)');
         $('.Adv_bar').eq(0).css('background-color','#6e6d6d');
         $('.Adv_bar').eq(2).css('background-color','#6e6d6d');
         });
     $('.Adv_bar').eq(2).click(function () {
-        $('.slide_1').css('margin-left', '-248%' );
+        $('.slide_1').css('margin-left', '-200%' );
+        $('.slide_2').css('margin-left', '-200%' );
+        $('.slide_3').css('margin-left', '-200%' );
         $('.Adv_bar').eq(2).css('background-color','rgba(11, 171, 177, 0.815)');
         $('.Adv_bar').eq(0).css('background-color','#6e6d6d');
         $('.Adv_bar').eq(1).css('background-color','#6e6d6d');
